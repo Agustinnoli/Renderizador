226aaa8d8dd1bbe54fda8953821bba594516ac73
Agustinnoli <agustinnolivillar@gmail.com>
2026-01-12 03:45:12 -0300

    movimiento de puntos

--- graficos.c
+++ graficos.c
@@ -8,7 +8,9 @@
 #define COLOR_BLACK 0x00000000
 #define ANCHO 600
 #define ALTURA 600
-#define PIXELSIZE 2
+#define PIXELSIZE 10
+#define FPS 60
+
 SDL_Surface* surface;
 SDL_Window* window;
 
@@ -31,23 +33,24 @@ int x2;
 int y2;
 } linea_t;
 */
-void dibujarLinea(punto_t* p1, punto_t* p2) {
 
-    float x1 = (p1->x +1) * ANCHO/2;
-    float x2 = (p2->x +1) * ANCHO/2;
-    float y1 = (ALTURA/2)* (1-p1->y);
-    float y2 = (ALTURA/2) * (1-p2->y);
+
+
+void dibujarLinea(punto_t p1, punto_t p2) {
+
+    float x1 = ((p1.x + 1) * ANCHO - PIXELSIZE)/2;
+    float x2 = ((p2.x + 1) * ANCHO - PIXELSIZE)/2;
+    float y1 = ((1 - p1.y) * ALTURA - PIXELSIZE)/2;
+    float y2 = ((1 - p2.y) * ALTURA - PIXELSIZE)/2;
     SDL_Rect pixel;
-
-
     int xVectorDirector = (int) (x2 - x1);
     int yVectorDirector = (int) (y2 - y1);
     int CantPixelesADibujar;
     if (abs(xVectorDirector) > abs(yVectorDirector)){ CantPixelesADibujar = abs(xVectorDirector);}else{ CantPixelesADibujar = abs(yVectorDirector);}
     if (CantPixelesADibujar == 0) { pixel = (SDL_Rect) {(int)x1, (int)y1, PIXELSIZE, PIXELSIZE};SDL_FillRect(surface, &pixel, COLOR_WHITE);return;}
     //la componente mas larga queda en 1, asi se puede sumar de a 1  
-    float xNormalizado = xVectorDirector / CantPixelesADibujar;
-    float yNormalizado = yVectorDirector / CantPixelesADibujar;
+    float xNormalizado = (float) xVectorDirector / CantPixelesADibujar;
+    float yNormalizado = (float) yVectorDirector / CantPixelesADibujar;
     for (int i = 0; i <= CantPixelesADibujar; i++) {
         pixel = (SDL_Rect){(int)x1, (int)y1, PIXELSIZE, PIXELSIZE};
         SDL_FillRect(surface, &pixel, COLOR_WHITE);
@@ -56,36 +59,51 @@ void dibujarLinea(punto_t* p1, punto_t* p2) {
     }
 }
 
-
+punto_t alejar(punto_t p){
+    p.x = p.x/p.z;
+    p.y = p.y/p.z;
+    return p;
+}
 int main(){
     //setenv("SDL_VIDEODRIVER", "wayland", 1);
     SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, ALTURA, ANCHO, 0);
+    window = SDL_CreateWindow("Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,ANCHO , ALTURA, 0);
     surface = SDL_GetWindowSurface(window);
     SDL_Rect rect = (SDL_Rect) {200, 200, 200, 200};
     SDL_Rect fondo = (SDL_Rect) {0, 0, 900, 600};
     bool running = true;
     SDL_Event event;
+    punto_t p1 = (punto_t){0.5,0.5,1};
+    punto_t p2 = (punto_t){-0.5,0.5,1};
+    punto_t p3 = (punto_t){0.5,-0.5,1};
+    punto_t p4 = (punto_t){-0.5,-0.5,1};
+    
+    //dibujarLinea(&p2,&p2);
+    //dibujarLinea(&p3,&p3);
+    //dibujarLinea(&p4,&p4);
+    float deltaTiempo = 1.0f /FPS;
     while(running){
         while(SDL_PollEvent(&event)){
             if (event.type == SDL_QUIT){
                 running = false;
             }
             if (event.type == SDL_MOUSEMOTION && event.motion.state != 0){
-                rect.x = event.motion.x - 100;
-                rect.y = event.motion.y - 100;
+                //rect.x = event.motion.x - 100;
+                //rect.y = event.motion.y - 100;
         
             }
-        //SDL_FillRect(surface, &fondo, COLOR_BLACK);    
-        //SDL_FillRect(surface, &rect, COLOR_WHITE);
-        //linea_t* linea = crearLinea(1,2,10,20);
-        punto_t p1 = (punto_t){0,0,0};
-        punto_t p2 = (punto_t){0,0,0};
-        dibujarLinea(&p1,&p2);
-        SDL_UpdateWindowSurface(window);
-        SDL_Delay(10);
-
         }
+        p1.z += deltaTiempo;
+        p2.z += deltaTiempo;
+        p3.z += deltaTiempo;
+        p4.z += deltaTiempo;
+        dibujarLinea(alejar(p1),alejar(p2));
+        dibujarLinea(alejar(p2),alejar(p4));
+        dibujarLinea(alejar(p3),alejar(p4));
+        dibujarLinea(alejar(p1),alejar(p3));
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(16); //60FsPS
+        SDL_FillRect(surface, &fondo, COLOR_BLACK); 
     }
     return 0;
 }
