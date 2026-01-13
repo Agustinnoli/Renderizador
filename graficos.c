47ba04ce192f8cefcce0f6ea94c40dc9f3f23bfe
Agustinnoli <agustinnolivillar@gmail.com>
2026-01-13 06:20:34 -0300

    cubo dando vueltas

--- graficos.c
+++ graficos.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define COLOR_WHITE 0xffffffff
 #define COLOR_BLACK 0x00000000
@@ -10,7 +11,8 @@
 #define ALTURA 600
 #define PIXELSIZE 10
 #define FPS 60
-
+#define DELTATIEMPO 1.0f/FPS
+#define PI 3.14159
 SDL_Surface* surface;
 SDL_Window* window;
 
@@ -37,11 +39,15 @@ int y2;
 
 
 void dibujarLinea(punto_t p1, punto_t p2) {
-
-    float x1 = ((p1.x + 1) * ANCHO - PIXELSIZE)/2;
-    float x2 = ((p2.x + 1) * ANCHO - PIXELSIZE)/2;
-    float y1 = ((1 - p1.y) * ALTURA - PIXELSIZE)/2;
-    float y2 = ((1 - p2.y) * ALTURA - PIXELSIZE)/2;
+    
+    float x1 = ((p1.x + 1) * ANCHO - PIXELSIZE)/2; 
+    float x2 = ((p2.x + 1) * ANCHO - PIXELSIZE)/2; 
+    float y1 = ((1 - p1.y) * ALTURA - PIXELSIZE)/2; 
+    float y2 = ((1 - p2.y) * ALTURA - PIXELSIZE)/2; 
+    //float x1 = ((p1->x + 1) * ANCHO - PIXELSIZE)/2;
+    //float x2 = ((p2->x + 1) * ANCHO - PIXELSIZE)/2;
+    // float y1 = ((1 - p1->y) * ALTURA - PIXELSIZE)/2;
+    //float y2 = ((1 - p2->y) * ALTURA - PIXELSIZE)/2;
     SDL_Rect pixel;
     int xVectorDirector = (int) (x2 - x1);
     int yVectorDirector = (int) (y2 - y1);
@@ -62,8 +68,57 @@ void dibujarLinea(punto_t p1, punto_t p2) {
 punto_t alejar(punto_t p){
     p.x = p.x/p.z;
     p.y = p.y/p.z;
+    //p->x = p->x/p->z;
+    //p->y = p->y/p->z;
     return p;
+
 }
+//punto_t proyectarZ(punto_t p){
+//    p.z += DELTATIEMPO;
+//    //p->z += DELTATIEMPO;
+//    return p;
+//}
+punto_t puntos[] ={
+
+{0.5,0.5,0.5},
+{-0.5,0.5,0.5},
+{-0.5,-0.5,0.5},
+{0.5,-0.5,0.5},
+{0.5,0.5,-0.5},
+{-0.5,0.5,-0.5},
+{-0.5,-0.5,-0.5},
+{0.5,-0.5,-0.5}
+};
+
+int poligonos[][4] = {
+{0,1,2,3}, 
+{4,5,6,7}, 
+{0,4,7,3}, 
+{1,5,6,2}, 
+{0,1,5,4}, 
+{3,2,6,7}  
+};
+
+
+punto_t rotarPlanoxz(punto_t p, float angulo) {
+    float temp = p.x; 
+    p.x = temp * cos(angulo) - p.z * sin(angulo);
+    p.z = temp * sin(angulo) + p.z * cos(angulo);
+    return p;
+
+}
+
+/*
+
+void rotarPlanoxz(punto_t* p, float angulo) {
+    float temp = p->x; 
+    p->x = temp * cos(angulo) - p->z * sin(angulo);
+    p->z = temp * sin(angulo) + p->z * cos(angulo);
+
+}
+*/
+
+
 int main(){
     //setenv("SDL_VIDEODRIVER", "wayland", 1);
     SDL_Init(SDL_INIT_VIDEO);
@@ -73,15 +128,11 @@ int main(){
     SDL_Rect fondo = (SDL_Rect) {0, 0, 900, 600};
     bool running = true;
     SDL_Event event;
-    punto_t p1 = (punto_t){0.5,0.5,1};
-    punto_t p2 = (punto_t){-0.5,0.5,1};
-    punto_t p3 = (punto_t){0.5,-0.5,1};
-    punto_t p4 = (punto_t){-0.5,-0.5,1};
-    
     //dibujarLinea(&p2,&p2);
     //dibujarLinea(&p3,&p3);
     //dibujarLinea(&p4,&p4);
-    float deltaTiempo = 1.0f /FPS;
+    float angulo = 0;
+    float dz = 1;
     while(running){
         while(SDL_PollEvent(&event)){
             if (event.type == SDL_QUIT){
@@ -93,16 +144,29 @@ int main(){
         
             }
         }
-        p1.z += deltaTiempo;
-        p2.z += deltaTiempo;
-        p3.z += deltaTiempo;
-        p4.z += deltaTiempo;
-        dibujarLinea(alejar(p1),alejar(p2));
-        dibujarLinea(alejar(p2),alejar(p4));
-        dibujarLinea(alejar(p3),alejar(p4));
-        dibujarLinea(alejar(p1),alejar(p3));
+        angulo += 2*PI*DELTATIEMPO;
+        dz += DELTATIEMPO;
+        for(int i = 0; i < (sizeof(puntos)/sizeof(punto_t)); i ++){
+            punto_t p = puntos[i];
+            p = rotarPlanoxz(p,angulo);
+            p.z += 2.0f;//dz;    
+            dibujarLinea(alejar(p),alejar(p));
+        }
+        for(int i =0 ; i <(sizeof(poligonos)/sizeof(int[4])); i ++ ){
+            for(int j = 0; j < 4; j ++){
+                punto_t p1 = puntos[poligonos[i][j]];
+                punto_t p2 = puntos[poligonos[i][(j+1) % 4]];
+                p1 = rotarPlanoxz(p1,angulo);
+                p2 = rotarPlanoxz(p2,angulo);
+                p1.z += 2.0f;//dz;
+                p2.z += 2.0f;//dz;    
+                dibujarLinea(alejar(p1),alejar(p2));
+        }
+
+        }   
+
         SDL_UpdateWindowSurface(window);
-        SDL_Delay(16); //60FsPS
+        SDL_Delay(16); //60FPS
         SDL_FillRect(surface, &fondo, COLOR_BLACK); 
     }
     return 0;
