706ff8c44d554bbba6474f8c678d1cdfdee75bbd
Agustinnoli <agustinnolivillar@gmail.com>
2026-01-22 22:50:32 -0300

    clip 3D y 2D

--- graficos.c
+++ graficos.c
@@ -28,8 +28,17 @@ float y;
 float z;
 
 } punto_t;
-punto_t* copia;
 
+
+typedef struct linea {
+punto_t p1;
+punto_t p2;
+} linea_t;
+
+
+punto_t* copia;
+linea_t* lineasADibujar;
+int tamaniolineasADibujar;
 
 
 void dibujarLinea(punto_t* p1, punto_t* p2) {
@@ -42,18 +51,33 @@ void dibujarLinea(punto_t* p1, punto_t* p2) {
     int xVectorDirector = (int) (x2 - x1);
     int yVectorDirector = (int) (y2 - y1);
     int CantPixelesADibujar;
+
+    float xVectorDirector2 = - xVectorDirector;
+    float yVectorDirector2 = - yVectorDirector;
+
+    if(x1 > ANCHO){float mult = (ANCHO - x1)/xVectorDirector2; x1 = mult*xVectorDirector2 + x1;y1= mult*yVectorDirector2 + y1; }
+    else if(x2 > ANCHO){float mult = (ANCHO - x2)/xVectorDirector; x2 = mult*xVectorDirector + x2;y2= mult*yVectorDirector + y2; }
+    if(x1 < 0){float mult = (- x1)/xVectorDirector2; x1 = mult*xVectorDirector2 + x1;y1= mult*yVectorDirector2 + y1; }
+    else if(x2 < 0){float mult = (- x2)/xVectorDirector; x2 = mult*xVectorDirector + x2;y2= mult*yVectorDirector + y2; }
+    if(y1 > ALTURA){float mult = (ALTURA - y1)/yVectorDirector2; x1 = mult*xVectorDirector2 + x1;y1= mult*yVectorDirector2 + y1; }
+    else if(y2 > ALTURA){float mult = (ALTURA - y2)/yVectorDirector; x2 = mult*xVectorDirector + x2;y2= mult*yVectorDirector + y2; }
+    if(y1 < 0){float mult = (- y1)/yVectorDirector2; x1 = mult*xVectorDirector2 + x1;y1= mult*yVectorDirector2 + y1; }
+    else if(y2 < 0){float mult = (- y2)/yVectorDirector; x2 = mult*xVectorDirector + x2;y2= mult*yVectorDirector + y2; }
+    xVectorDirector = (int) (x2 - x1);
+    yVectorDirector = (int) (y2 - y1);
+
     if (abs(xVectorDirector) > abs(yVectorDirector)){ CantPixelesADibujar = abs(xVectorDirector);}else{ CantPixelesADibujar = abs(yVectorDirector);}
     if (CantPixelesADibujar == 0) { pixel = (SDL_Rect) {(int)x1, (int)y1, PIXELSIZE, PIXELSIZE};SDL_FillRect(surface, &pixel, COLOR_WHITE);return;}
     //la componente mas larga queda en 1, asi se puede sumar de a 1  
     float xNormalizado = (float) xVectorDirector / CantPixelesADibujar;
     float yNormalizado = (float) yVectorDirector / CantPixelesADibujar;
-    if(x1 <= 0 || x1 > ANCHO ||y1 <= 0 || y1 > ALTURA){ x1 = x2; y1 = y2; xNormalizado = -xNormalizado;yNormalizado = -yNormalizado;}
+
+
     for (int i = 0; i <= CantPixelesADibujar; i++) {
         pixel = (SDL_Rect){(int)x1, (int)y1, PIXELSIZE, PIXELSIZE};
         SDL_FillRect(surface, &pixel, COLOR_WHITE);
         x1 += xNormalizado;
         y1 += yNormalizado;
-        if(x1 < 0 || x1 > ANCHO ||y1 < 0 || y1 > ALTURA){ return;}// esto consume mucoh, deberia de calcular la intersecccion con el plano para saber hasta que punto deberia de  dibujarr
     } 
 }
 punto_t puntos[] ={
@@ -104,27 +128,56 @@ void generarRotacionYPasarACopia(){
     }
 }
 void alejarTodos(){
-    for(int i =0 ; i < sizeof(puntos)/sizeof(punto_t); i ++ ){
-        //if (copia[i].z<=NEARPLANE && copia[i].z>=0){copia[i].z = NEARPLANE;}
-        //if (copia[i].z>=-NEARPLANE && copia[i].z<=0){copia[i].z = -NEARPLANE;}
-        copia[i].x = copia[i].x/ copia[i].z;
-        copia[i].y = copia[i].y/ copia[i].z;
+    for(int i =0 ; i < tamaniolineasADibujar; i ++ ){
+        lineasADibujar[i].p1.x = lineasADibujar[i].p1.x / lineasADibujar[i].p1.z;
+        lineasADibujar[i].p1.y = lineasADibujar[i].p1.y / lineasADibujar[i].p1.z;
+        lineasADibujar[i].p2.x = lineasADibujar[i].p2.x / lineasADibujar[i].p2.z;
+        lineasADibujar[i].p2.y = lineasADibujar[i].p2.y / lineasADibujar[i].p2.z;
+
            
                   
     }
 
 }
 void dibujarPoligono(){
+    for (int i = 0; i < tamaniolineasADibujar; i++){
+        punto_t *p1 = &lineasADibujar[i].p1;
+        punto_t *p2 = &lineasADibujar[i].p2;
+        dibujarLinea(p1, p2);
+
+    }
+}
+void clip3D(){
+    tamaniolineasADibujar =0;
     for (int i = 0; i < (sizeof(poligonos) / sizeof(int[3])); i++){
-        punto_t *p1 = &copia[poligonos[i][0]];
-        punto_t *p2 = &copia[poligonos[i][1]];
-        punto_t *p3 = &copia[poligonos[i][2]];
-        if(!(p1->z < 0.1 && p2->z < 0.1 && p3->z < 0.1) ){ // funciona con lo que debe de hacer pero au asi falta porner los intermedio tipo si un punto quedo afuera y los otros 2 adentro 
-            dibujarLinea(p1, p2);
-            dibujarLinea(p2, p3);
-            dibujarLinea(p3, p1);
+        for (int j =0 ; j < 3; j ++){
+            punto_t *p1 = &copia[poligonos[i][j]];
+            punto_t *p2 = &copia[poligonos[i][(j +1)%3]];
+            if(p1->z < NEARPLANE && p2->z < NEARPLANE){continue;}
+            if(p1->z < NEARPLANE || p2->z < NEARPLANE){
+                if(p1->z < NEARPLANE){punto_t* temp = p2; p2 = p1; p1 = temp;}
+                punto_t vectorDirecotr = {p2->x-p1->x,p2->y-p1->y,p2->z-p1->z};
+                float Intersecccion = (NEARPLANE- p2->z )/vectorDirecotr.z;
+                punto_t puntoInterseccion ={vectorDirecotr.x*Intersecccion +p2->x,vectorDirecotr.y*Intersecccion +p2->y,vectorDirecotr.z*Intersecccion +p2->z};
+                p2 = &puntoInterseccion;
+            }
+            bool p1Arriba = p1->y > p1->z;
+            bool p1Abajo = p1->y < -p1->z;
+            bool p1Derecha = p1->x > p1->z;
+            bool p1Izquierda = p1->x < -p1->z;
+            bool p2Arriba = p2->y > p2->z;
+            bool p2Abajo = p2->y < -p2->z;
+            bool p2Derecha = p2->x > p2->z;
+            bool p2Izquierda = p2->x < -p2->z;
+            if(p1Arriba&&p2Arriba){continue;}
+            if(p1Abajo&&p2Abajo){continue;}
+            if(p1Derecha&&p2Derecha){continue;}
+            if(p1Izquierda&&p2Izquierda){continue;}
+            lineasADibujar[tamaniolineasADibujar] = (linea_t) {*p1,*p2};
+            tamaniolineasADibujar++;
         }
     }
+    
 }
 int main(){
     SDL_Init(SDL_INIT_VIDEO);
@@ -134,6 +187,7 @@ int main(){
     bool running = true;
     SDL_Event event;
     const Uint8 *teclado = SDL_GetKeyboardState(NULL);
+    lineasADibujar = malloc((sizeof(linea_t)*sizeof(poligonos)*3)/sizeof(int[3]));
     copia = malloc(sizeof(puntos));
     while(running){
         while(SDL_PollEvent(&event)){
@@ -157,7 +211,9 @@ int main(){
 
         generarRotacionYPasarACopia();
         ajustar();
+        clip3D();
         alejarTodos();
+        
         dibujarPoligono();
 
         SDL_PumpEvents();
@@ -166,6 +222,7 @@ int main(){
         SDL_FillRect(surface, &fondo, COLOR_BLACK); 
     }
     free(copia);
+    free(lineasADibujar);
     return 0;
 }
 
