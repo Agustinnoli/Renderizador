4bfcd2abecfcde89b0b5033019c4cefba99a67f2
Agustin Fernando Noli Villar <agustinnolivillar@gmail.com>
2026-06-22 02:06:07 -0300

    refactors

--- graficos.c
+++ graficos.c
@@ -21,30 +21,37 @@ float dz = 2;
 float dx = 0;
 float dy = 0;
 
-typedef struct punto {
+typedef struct punto3D {
 float x;
 float y;
 float z;
 
-} punto_t;
+} punto3D_t;
+typedef struct punto2D {
+int x;
+int y;
+} punto2D_t;
 
 
-typedef struct linea {
-punto_t p1;
-punto_t p2;
-} linea_t;
-typedef struct poligono {
-punto_t p1;
-punto_t p2;
-punto_t p3;
-} poligono_t;
+typedef struct poligono3D {
+punto3D_t p1;
+punto3D_t p2;
+punto3D_t p3;
+} poligono3D_t;
 
-punto_t* copia;
-poligono_t* poligonosADibujar;
+typedef struct poligono2D {
+punto2D_t p1;
+punto2D_t p2;
+punto2D_t p3;
+} poligono2D_t;
 
-int tamanioPoligonosADibujar;
+poligono3D_t* poligonosRotados;
+poligono3D_t* poligonosClipeados;
+poligono2D_t* poligonosADibujar;
 
-punto_t puntos[] = {
+int tamanioPoligonosClipeados;
+
+punto3D_t vertices[] = {
     {-0.5f, -0.5f, -0.5f}, 
     { 0.5f, -0.5f, -0.5f}, 
     { 0.5f,  0.5f, -0.5f}, 
@@ -74,53 +81,63 @@ float* zbuffer;
 
 
 void ajustar(){
-    for(int i =0 ; i < sizeof(puntos)/sizeof(punto_t); i ++ ){
-        copia[i].x += dx;
-        copia[i].y += dy;
-        copia[i].z += dz;
+    for(int i =0 ; i < sizeof(poligonos) / sizeof(poligonos[0]); i ++ ){
+        punto3D_t* puntos = (punto3D_t*)&poligonosRotados[i];
+        for (int j = 0; j < 3; j++) {
+            puntos[j].x += dx;
+            puntos[j].y += dy;
+            puntos[j].z += dz;
+        }
     }
 }
-void generarRotacionYPasarACopia(){
-        float seno = sin(angulo);
-        float coseno = cos(angulo);
-    for(int i =0 ; i < sizeof(puntos)/sizeof(punto_t); i ++ ){
-        
-        copia[i].x =puntos[i].x * coseno - puntos[i].z * seno;
-        copia[i].y =puntos[i].y;
-        copia[i].z =puntos[i].x * seno + puntos[i].z * coseno;
-        //copia[i].x =puntos[i].x * cos(angulo) - puntos[i].y * sin(angulo);
-        //copia[i].y =puntos[i].x * sin(angulo) + puntos[i].y * cos(angulo);
-        //copia[i].z =puntos[i].z;
-        //copia[i].x =puntos[i].x;
-        //copia[i].y =puntos[i].y * cos(angulo) - puntos[i].z * sin(angulo);
-        //copia[i].z =puntos[i].y * sin(angulo) + puntos[i].z * cos(angulo);
+void generarRotacion(){
+    float seno = sin(angulo);
+    float coseno = cos(angulo);
+    for(int i =0 ; i < sizeof(poligonos) / sizeof(poligonos[0]); i ++ ){
+        punto3D_t* puntos = (punto3D_t*)&poligonosRotados[i];
+        for (int j = 0; j < 3; j++) {
+            puntos[j].x = vertices[poligonos[i][j]].x * coseno - vertices[poligonos[i][j]].z * seno;
+            puntos[j].y = vertices[poligonos[i][j]].y;
+            puntos[j].z = vertices[poligonos[i][j]].x * seno + vertices[poligonos[i][j]].z * coseno;
+        }
     }
+        //poligonosRotados[i].x =vertices[i].x * cos(angulo) - vertices[i].y * sin(angulo);
+        //poligonosRotados[i].y =vertices[i].x * sin(angulo) + vertices[i].y * cos(angulo);
+        //poligonosRotados[i].z =vertices[i].z;
+        //poligonosRotados[i].x =vertices[i].x;
+        //poligonosRotados[i].y =vertices[i].y * cos(angulo) - vertices[i].z * sin(angulo);
+        //poligonosRotados[i].z =vertices[i].y * sin(angulo) + vertices[i].z * cos(angulo);
 }
-void alejarTodos(){
-    for(int i =0 ; i < tamanioPoligonosADibujar; i ++ ){
-        poligonosADibujar[i].p1.x = poligonosADibujar[i].p1.x / poligonosADibujar[i].p1.z;
-        poligonosADibujar[i].p1.y = poligonosADibujar[i].p1.y / poligonosADibujar[i].p1.z;
-        poligonosADibujar[i].p2.x = poligonosADibujar[i].p2.x / poligonosADibujar[i].p2.z;
-        poligonosADibujar[i].p2.y = poligonosADibujar[i].p2.y / poligonosADibujar[i].p2.z;
-        poligonosADibujar[i].p3.x = poligonosADibujar[i].p3.x / poligonosADibujar[i].p3.z;
-        poligonosADibujar[i].p3.y = poligonosADibujar[i].p3.y / poligonosADibujar[i].p3.z;
-           
-                  
-    }
+punto2D_t coordsAPantalla(float x, float y){
+    return (punto2D_t) {(int)((x + 1) * ANCHO - PIXELSIZE)/2,(int) ((1 - y) * ALTURA - PIXELSIZE)/2} ;  
+}
+void proyectarAPantalla(){
 
+    for(int i =0 ; i < tamanioPoligonosClipeados; i ++ ){
+        punto2D_t* pts_dibujar = (punto2D_t*) &poligonosADibujar[i].p1;
+        punto3D_t* pts_clipeados = (punto3D_t*)&poligonosClipeados[i];
+        for (int j = 0; j < 3; j++) {
+            float x =  pts_clipeados[j].x / pts_clipeados[j].z;
+            float y =  pts_clipeados[j].y / pts_clipeados[j].z;
+            pts_dibujar[j] = coordsAPantalla(x,y);
+        }
+    }
 }
 void dibujarPoligono(){
     if (SDL_LockSurface(surface) != 0) {return;}
     size_t ancho_real_memoria = surface->pitch / 4;
     Uint32 *pixels = (Uint32 *)surface->pixels;
     srand(0); 
-    for (int i = 0; i < tamanioPoligonosADibujar; i++){
+    for (int i = 0; i < tamanioPoligonosClipeados; i++){
         //Uint32 color = rand();
         Uint32 color = i*10000 +10000;
 
-        punto_t *p1 = &poligonosADibujar[i].p1;
-        punto_t *p2 = &poligonosADibujar[i].p2;
-        punto_t *p3 = &poligonosADibujar[i].p3;
+        punto2D_t *p1 = &poligonosADibujar[i].p1;
+        punto2D_t *p2 = &poligonosADibujar[i].p2;
+        punto2D_t *p3 = &poligonosADibujar[i].p3;
+        float z1 = poligonosClipeados[i].p1.z;
+        float z2 = poligonosClipeados[i].p2.z;
+        float z3 = poligonosClipeados[i].p3.z;
        
         int x1 = (int) p1->x;
         int y1 = (int) p1->y;
@@ -128,7 +145,7 @@ void dibujarPoligono(){
         int y2 = (int) p2->y;
         int x3 = (int) p3->x;
         int y3 = (int) p3->y;
-        float z = (p1->z + p2->z + p3->z) / 3.0f;
+        float z = (z1 + z2 + z3) / 3.0f;
         
         int boundingBoxminX = (x1 < x2) ? ((x1 < x3) ? x1 : x3) : ((x2 < x3) ? x2 : x3);
         int boundingBoxminY = (y1 < y2) ? ((y1 < y3) ? y1 : y3) : ((y2 < y3) ? y2 : y3);
@@ -169,42 +186,38 @@ void dibujarPoligono(){
     SDL_UnlockSurface(surface);
 }
 
-punto_t jijona(punto_t* dentro, punto_t* afuera){
-        punto_t vectorDirecotr = {afuera->x-dentro->x,afuera->y-dentro->y,afuera->z-dentro->z};
+punto3D_t jijona(punto3D_t* dentro, punto3D_t* afuera){
+        punto3D_t vectorDirecotr = {afuera->x-dentro->x,afuera->y-dentro->y,afuera->z-dentro->z};
         float Intersecccion = (NEARPLANE- afuera->z )/vectorDirecotr.z;
-        //afuera->x = vectorDirecotr.x*Intersecccion +afuera->x;
-        //afuera->y =vectorDirecotr.y*Intersecccion +afuera->y;
-        //afuera->z = NEARPLANE;
-        return (punto_t) {vectorDirecotr.x*Intersecccion +afuera->x,vectorDirecotr.y*Intersecccion +afuera->y,NEARPLANE};
-        
+        return (punto3D_t) {vectorDirecotr.x*Intersecccion +afuera->x,vectorDirecotr.y*Intersecccion +afuera->y,NEARPLANE};
 }
 
-bool checkeoFueraPantallaPoligono3D(punto_t* p1,punto_t* p2,punto_t* p3){
-    bool p1Arriba = p1->y > p1->z;
-    bool p1Abajo = p1->y < -p1->z;
-    bool p1Derecha = p1->x > p1->z;
-    bool p1Izquierda = p1->x < -p1->z;
-    bool p2Arriba = p2->y > p2->z;
-    bool p2Abajo = p2->y < -p2->z;
-    bool p2Derecha = p2->x > p2->z;
-    bool p2Izquierda = p2->x < -p2->z;
-    bool p3Arriba = p3->y > p3->z;
-    bool p3Abajo = p3->y < -p3->z;
-    bool p3Derecha = p3->x >  p3->z;
-    bool p3Izquierda = p3->x < -p3->z;
-    return(p1Arriba&&p2Arriba&&p3Arriba)||(p1Abajo&&p2Abajo&&p3Abajo)||(p1Derecha&&p2Derecha&&p3Derecha)||(p1Izquierda&&p2Izquierda&&p3Izquierda);    
+bool checkeoFueraPantallaPoligono3D(punto3D_t* p1,punto3D_t* p2,punto3D_t* p3){
+    bool p1Arriba = p1->y > p1->z;bool p2Arriba = p2->y > p2->z;bool p3Arriba = p3->y > p3->z;
+    bool estaArriba = p1Arriba&&p2Arriba&&p3Arriba;
+
+    bool p1Abajo = p1->y < -p1->z;bool p2Abajo = p2->y < -p2->z;bool p3Abajo = p3->y < -p3->z;
+    bool estaAbajo = p1Abajo&&p2Abajo&&p3Abajo;
+
+    bool p1Derecha = p1->x > p1->z;bool p2Derecha = p2->x > p2->z;bool p3Derecha = p3->x >  p3->z;
+    bool estaDerecha =p1Derecha&&p2Derecha&&p3Derecha;
+
+    bool p1Izquierda = p1->x < -p1->z;bool p2Izquierda = p2->x < -p2->z;bool p3Izquierda = p3->x < -p3->z; 
+    bool estaIzqiuerda = p1Izquierda&&p2Izquierda&&p3Izquierda;
+
+    return estaArriba||estaAbajo||estaDerecha||estaIzqiuerda;    
 }
 void clip3D(){
-    tamanioPoligonosADibujar =0;
+    tamanioPoligonosClipeados =0;
     int yay;
-    punto_t* temp;
-    punto_t temp2;
-    punto_t temp3;
+    punto3D_t* temp;
+    punto3D_t temp2;
+    punto3D_t temp3;
     for (int i = 0; i < (sizeof(poligonos) / sizeof(int[3])); i++){
 
-            punto_t *p1 = &copia[poligonos[i][0]];
-            punto_t *p2 = &copia[poligonos[i][1]];
-            punto_t *p3 = &copia[poligonos[i][2]];
+            punto3D_t *p1 = &poligonosRotados[i].p1;
+            punto3D_t *p2 = &poligonosRotados[i].p2;
+            punto3D_t *p3 = &poligonosRotados[i].p3;
             yay = 0;
             if(p1->z < NEARPLANE){ yay |= 0b001;}
             if(p2->z < NEARPLANE){ yay |= 0b010;}// esto podria ser un enum 
@@ -212,8 +225,8 @@ void clip3D(){
             switch (yay){
                 case 0b000://ninguno afuera
                     if(checkeoFueraPantallaPoligono3D(p1,p2,p3)){continue;}
-                    poligonosADibujar[tamanioPoligonosADibujar] = (poligono_t) {*p1,*p2,*p3};
-                    tamanioPoligonosADibujar++;
+                    poligonosClipeados[tamanioPoligonosClipeados] = (poligono3D_t) {*p1,*p2,*p3};
+                    tamanioPoligonosClipeados++;
                 continue;
                 break;
                 case 0b001://p1 afuera
@@ -245,41 +258,28 @@ void clip3D(){
             caso1Afuera: // asumo que p1 esta afuera
                 temp2 = jijona(p3,p1);
                 if(!checkeoFueraPantallaPoligono3D(&temp2,p2,p3)){
-                    poligonosADibujar[tamanioPoligonosADibujar] = (poligono_t) {temp2,*p2,*p3};
-                    tamanioPoligonosADibujar++;
+                    poligonosClipeados[tamanioPoligonosClipeados] = (poligono3D_t) {temp2,*p2,*p3};
+                    tamanioPoligonosClipeados++;
                 }
                 temp3 = jijona(p2,p1);
                 if(checkeoFueraPantallaPoligono3D(&temp3,p2,&temp2)){continue;}
-                poligonosADibujar[tamanioPoligonosADibujar] = (poligono_t) {temp3,*p2,temp2};
-                tamanioPoligonosADibujar++;
+                poligonosClipeados[tamanioPoligonosClipeados] = (poligono3D_t) {temp3,*p2,temp2};
+                tamanioPoligonosClipeados++;
                 continue; 
 
             caso2Afuera: // asumo p1 p2 afuera;
                 temp2 = jijona(p3,p1);
                 temp3 = jijona(p3,p2);
                 if(checkeoFueraPantallaPoligono3D(&temp3,&temp2,p3)){continue;}
-                poligonosADibujar[tamanioPoligonosADibujar] = (poligono_t) {temp3,temp2,*p3};
-                tamanioPoligonosADibujar++;
+                poligonosClipeados[tamanioPoligonosClipeados] = (poligono3D_t) {temp3,temp2,*p3};
+                tamanioPoligonosClipeados++;
                 continue;
         
     }
     
 }
 
-void coordsAPantalla(){
-    for (int i = 0; i < tamanioPoligonosADibujar; i++){
-        poligonosADibujar[i].p1.x = ((poligonosADibujar[i].p1.x + 1) * ANCHO - PIXELSIZE)/2;
-        poligonosADibujar[i].p1.y = ((1 - poligonosADibujar[i].p1.y) * ALTURA - PIXELSIZE)/2;
-        
-        poligonosADibujar[i].p2.x =((poligonosADibujar[i].p2.x + 1) * ANCHO - PIXELSIZE)/2;
-        poligonosADibujar[i].p2.y =((1 - poligonosADibujar[i].p2.y) * ALTURA - PIXELSIZE)/2;
-        
-        poligonosADibujar[i].p3.x =((poligonosADibujar[i].p3.x + 1) * ANCHO - PIXELSIZE)/2;
-        poligonosADibujar[i].p3.y =((1 - poligonosADibujar[i].p3.y) * ALTURA - PIXELSIZE)/2;
 
-    }
-
-}
 
 int main(){
     SDL_Init(SDL_INIT_VIDEO);
@@ -289,13 +289,12 @@ int main(){
     bool running = true;
     SDL_Event event;
     const Uint8 *teclado = SDL_GetKeyboardState(NULL);
-    //lineasADibujar = malloc((sizeof(linea_t)*sizeof(poligonos)*3)/sizeof(int[3]));
-    poligonosADibujar = malloc((sizeof(poligono_t)*sizeof(poligonos)*2)/sizeof(int[3]));
-    
+    poligonosClipeados = malloc((sizeof(poligono3D_t)*sizeof(poligonos)*2)/sizeof(int[3]));
+    poligonosADibujar = malloc((sizeof(poligono2D_t)*sizeof(poligonos)*2)/sizeof(int[3]));
     size_t ancho_real_memoria = surface->pitch / 4;
     zbuffer = malloc(sizeof(float)*ancho_real_memoria*ALTURA);
     
-    copia = malloc(sizeof(puntos));
+    poligonosRotados = malloc(sizeof(poligono3D_t) * (sizeof(poligonos) / sizeof(poligonos[0])));
     while(running){
         while(SDL_PollEvent(&event)){
             if (event.type == SDL_QUIT){
@@ -316,13 +315,12 @@ int main(){
         if (teclado[SDL_SCANCODE_LEFT]){angulo += PI*DELTATIEMPO;}
         if (teclado[SDL_SCANCODE_RIGHT]){angulo -= PI*DELTATIEMPO;}
 
-        for(int i = 0; i< ALTURA*ancho_real_memoria; i++){zbuffer[i]= 999999.0f;}
+        for(int i = 0; i< ALTURA*ancho_real_memoria; i++){zbuffer[i]= INFINITY;}
 
-        generarRotacionYPasarACopia();
+        generarRotacion();
         ajustar();
         clip3D();
-        alejarTodos();
-        coordsAPantalla();
+        proyectarAPantalla();
         
         dibujarPoligono();
 
@@ -331,8 +329,10 @@ int main(){
         SDL_Delay(16); //60FPS
         SDL_FillRect(surface, &fondo, COLOR_BLACK); 
     }
-    free(copia);
+    free(poligonosRotados);
+    free(poligonosClipeados);
     free(poligonosADibujar);
+    free(zbuffer);
     return 0;
 }
 
